fac1e1363ed19ea239b45fef2e1cee16960f1ecc
jholdstock <jholdstock@decred.org>
2021-09-10 10:57:28 +0100

    Document usage of Hugo binary.
    Use long flag names and include short doco for each one.
    
    Also adding `--buildFuture` which will ensure any sections or pages with a publishDate in the future are included in the site.

--- bin/watch.sh
+++ bin/watch.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-hugo server -F -D --disableFastRender -b http://localhost:1313
+# Remove old hugo output before building
+rm -rf src/public src/resources
+
+# Serve site
+#   --buildFuture          include content with publishdate in the future
+#   --buildDrafts          include content marked as draft
+#   --disableFastRender    enables full re-renders on changes
+#   --source               filesystem path to read files relative from
+#   --baseURL              hostname (and path) to the root
+hugo server \
+    --buildFuture \
+    --buildDrafts \
+    --disableFastRender \
+    --source src \
+    --baseURL http://localhost:1313
