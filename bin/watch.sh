26ff8952e017f5d8eea82e5554f4d6c16d8c6e07
Jamie Holdstock <jholdstock@decred.org>
2023-03-23 15:00:32 +0000

    Use hugo 0.110.0. (#46)
    * Use hugo 0.110.0.
    
    Nothing much included in recent versions, mostly bug-fixes and build/CI improvements. The binary name has changed so thats updated in our Dockerfile.
    
    * Fix watch script.
    
    This repo doesnt use a src/ directory, everything is just in the repo root.
    
    * Hard-code MIME type for RSS feed.

--- bin/watch.sh
+++ bin/watch.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Remove old hugo output before building
-rm -rf src/public src/resources
+rm -rf public resources
 
 # Serve site
 #   --buildFuture          include content with publishdate in the future
 #   --buildDrafts          include content marked as draft
 #   --disableFastRender    enables full re-renders on changes
-#   --source               filesystem path to read files relative from
 #   --baseURL              hostname (and path) to the root
 hugo server \
     --buildFuture \
     --buildDrafts \
     --disableFastRender \
-    --source src \
     --baseURL http://localhost:1313
